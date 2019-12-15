75ce5f1b0e2a056334ebcef2c657635cec18c44e
dhsekf123 <56681104+dhsekf123@users.noreply.github.com>
2019-12-15 23:20:13 +0900

    질문 확인

--- delete.php
+++ delete.php
@@ -1,14 +1,16 @@
 <?
+   session_start();
+
    include "../lib/dbconn.php";
 
-   $sql = "delete from memo where num = $num";
+   $sql = "delete from $table where num = $num";
    mysql_query($sql, $connect);
 
    mysql_close();
 
    echo "
 	   <script>
-	    location.href = 'memo.php';
+	    location.href = 'list.php?table=$table';
 	   </script>
 	";
 ?>
