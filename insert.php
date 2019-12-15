ae1db3bb00f6e2fd024f05251a31abeb5aff7639
dhsekf123 <56681104+dhsekf123@users.noreply.github.com>
2019-12-15 23:19:41 +0900

    메모 확인

--- insert.php
+++ insert.php
@@ -11,16 +11,6 @@
 		exit;
 	}
 
-	if(!$subject) {
-		echo("
-	   <script>
-	     window.alert('제목을 입력하세요.')
-	     history.go(-1)
-	   </script>
-		");
-	 exit;
-	}
-
 	if(!$content) {
 		echo("
 	   <script>
@@ -32,34 +22,26 @@
 	}
 
 	$regist_day = date("Y-m-d (H:i)");  // 현재의 '년-월-일-시-분'을 저장
+
 	include "../lib/dbconn.php";       // dconn.php 파일을 불러옴
 
-	if ($mode=="modify")
-	{
-		$sql = "update greet set subject='$subject', content='$content' where num=$num";
-	}
-	else
-	{
-		if ($html_ok=="y")
-		{
-			$is_html = "y";
-		}
-		else
-		{
-			$is_html = "";
-			$content = htmlspecialchars($content);
-		}
+    $sql = "select * from member where id='$userid'";
+    $result = mysql_query($sql, $connect);
+	$row = mysql_fetch_array($result);
 
-		$sql = "insert into greet (id, name, nick, subject, content, regist_day, hit, is_html) ";
-		$sql .= "values('$userid', '$username', '$usernick', '$subject', '$content', '$regist_day', 0, '$is_html')";
-	}
+	$name = $row[name];
+	$nick = $row[nick];
+
+	$sql = "insert into memo (id, name, nick, content, regist_day) ";
+	$sql .= "values('$userid', '$name', '$nick', '$content', '$regist_day')";
 
 	mysql_query($sql, $connect);  // $sql 에 저장된 명령 실행
+
 	mysql_close();                // DB 연결 끊기
 
 	echo "
 	   <script>
-	    location.href = 'list.php?page=$page';
+	    location.href = 'memo.php';
 	   </script>
 	";
 ?>
--- insert_ripple.php
+++ insert_ripple.php
@@ -11,21 +11,40 @@
 	   </script>
 	 ");
 	 exit;
-   }   
+   }
+   
+   if(!$ripple_content) {
+     echo("
+	   <script>
+	     window.alert('내용을 입력하세요.')
+	     history.go(-1)
+	   </script>
+	 ");
+	 exit;
+   }
+   
    include "../lib/dbconn.php";       // dconn.php 파일을 불러옴
+
+   $sql = "select * from member where id='$userid'";
+   $result = mysql_query($sql, $connect);
+   $row = mysql_fetch_array($result);
+
+   $name = $row[name];
+   $nick = $row[nick];
 
    $regist_day = date("Y-m-d (H:i)");  // 현재의 '년-월-일-시-분'을 저장
 
    // 레코드 삽입 명령
-   $sql = "insert into free_ripple (parent, id, name, nick, content, regist_day) ";
-   $sql .= "values($num, '$userid', '$username', '$usernick', '$ripple_content', '$regist_day')";    
+   $sql = "insert into memo_ripple (parent, id, name, nick, content, regist_day) ";
+   $sql .= "values($num, '$userid', '$name', '$nick', '$ripple_content', '$regist_day')";    
    
    mysql_query($sql, $connect);  // $sql 에 저장된 명령 실행
-   mysql_close();                // DB 연결 끊기
 
+   mysql_close();                // DB 연결 끊기
+   
    echo "
 	   <script>
-	    location.href = 'view.php?table=$table&num=$num';
+	    location.href = 'memo.php';
 	   </script>
 	";
 ?>
