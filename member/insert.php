8a16f50d1526c1b96215bbba2cd71b811bc1deec
dhsekf123 <56681104+dhsekf123@users.noreply.github.com>
2019-12-15 23:17:50 +0900

    회원 확인파일

---
+++ member/insert.php
@@ -0,0 +1,43 @@
+﻿<meta charset="utf-8">
+<?
+   $hp = $hp1."-".$hp2."-".$hp3;
+   $email = $email1."@".$email2;
+
+   $regist_day = date("Y-m-d (H:i)");  // 현재의 '년-월-일-시-분'을 저장
+   $ip = $REMOTE_ADDR;         // 방문자의 IP 주소를 저장
+
+   include "../lib/dbconn.php";       // dconn.php 파일을 불러옴               // C
+언어의 #include 와 같이 파일명 에 해당하는 파일을 현재의 위치에 삽입한다 만약
+경로가 표시되지 않았다면 , 삽입하려는 파일이 include 를 사용한 파일과 동일한 폴더에
+존재하는 것으로 간주한다
+
+   $sql = "select * from member where id='$id'";
+   $result = mysql_query($sql, $connect);
+   $exist_id = mysql_num_rows($result);
+
+   if($exist_id) {
+     echo("
+           <script>
+             window.alert('해당 아이디가 존재합니다.')
+             history.go(-1)
+           </script>
+         ");
+         exit;
+   }
+   else
+   {            // 레코드 삽입 명령을 $sql에 입력
+	    $sql = "insert into member(id, pass, name, nick, hp, email, regist_day, level) ";
+		$sql .= "values('$id', '$pass', '$name', '$nick', '$hp', '$email', '$regist_day', 9)";
+
+		mysql_query($sql, $connect);  // $sql 에 저장된 명령 실행
+   }
+
+   mysql_close();                // DB 연결 끊기
+   echo "
+	   <script>
+	    location.href = '../index.php';
+	   </script>
+	";
+?>
+
+   
